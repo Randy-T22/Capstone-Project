8655ec7eac84726e8505399e7c178e47cb7b9582
daelan <daelanhollingsworth205@gmail.com>
2022-04-04 15:58:20 -0500

    Added models[Expertise, Title, Language, Profile] to admin table

--- TrustApp/admin.py
+++ TrustApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Title)
+admin.site.register(Expertise)
+admin.site.register(Language)
+admin.site.register(Profile)
