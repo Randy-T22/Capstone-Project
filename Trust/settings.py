7fca43071a4f300d718ebd63107bfc4400d20306
daelan <daelanhollingsworth205@gmail.com>
2022-04-04 13:44:22 -0500

    trying to get static to work.

--- Trust/settings.py
+++ Trust/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TrustApp',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = ['TrustApp/static']
+
+STATIC_ROOT = '/TrustApp/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
