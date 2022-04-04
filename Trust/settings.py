f8d71830be6df63349dc171a70f41527616b52b3
daelan <daelanhollingsworth205@gmail.com>
2022-04-04 11:36:40 -0500

    changed so at least runs am changing models so uses user not custom user.

--- Trust/settings.py
+++ Trust/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +124,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.Employee'
