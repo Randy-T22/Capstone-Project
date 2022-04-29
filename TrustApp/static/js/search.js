3ff618dad164fc798f273c48676c111674de8fb2
Randy-T22 <89089916+Randy-T22@users.noreply.github.com>
2022-04-29 15:42:29 -0500

    * Linked JS to search.html * Updated search url

---
+++ TrustApp/static/js/search.js
@@ -0,0 +1,7 @@
+const users = document.getElementsByClassName("employeeBlock");
+const userNames = document.getElementsByClassName("name");
+for (var i = 0; i < users.length; i++) {
+  users[i].addEventListener("click", () => {
+    console.log("Button " + userNames + "has been clicked");
+  });
+}
--- TrustApp/static/search.js
+++
@@ -1,8 +0,0 @@
-
-
-var btn = document.getElementById("userbtn");
-btn.addEventListener("click", alertMe);
-
-function alertMe(){
-    console.log("The button has been clicked!");
-}
