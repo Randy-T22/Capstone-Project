910f09da50b2f58b003d374f6d83a731f2be5515
Logan Wilkins <luminexium460@gmail.com>
2022-05-02 11:57:20 -0500

    Fixed button issue

--- TrustApp/static/js/search.js
+++ TrustApp/static/js/search.js
@@ -1,7 +1,7 @@
 const users = document.getElementsByClassName("employeeBlock");
 const userNames = document.getElementsByClassName("name");
-for (var i = 0; i < users.length; i++) {
+for (let i = 0; i < users.length; i++) {
   users[i].addEventListener("click", () => {
-    console.log("Button " + users[i] + " has been clicked");
+    console.log("Button " + i + " has been clicked");
   });
 }
