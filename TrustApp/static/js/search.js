95b5b113ca58b27df812e3cee639e41953e1657a
Randy-T22 <89089916+Randy-T22@users.noreply.github.com>
2022-05-02 09:08:43 -0500

    * Shared current JS page

--- TrustApp/static/js/search.js
+++ TrustApp/static/js/search.js
@@ -2,6 +2,6 @@ const users = document.getElementsByClassName("employeeBlock");
 const userNames = document.getElementsByClassName("name");
 for (var i = 0; i < users.length; i++) {
   users[i].addEventListener("click", () => {
-    console.log("Button " + userNames + "has been clicked");
+    console.log("Button " + users[i] + " has been clicked");
   });
 }
